1df0f3f621335646caeeeb390da3db5dcdb4070b
Lin Aung <linaung.me@gmail.com>
2026-01-28 10:31:28 +0800

    Init

---
+++ src/webparts/chatKitAgent/components/IChatKitAgentProps.ts
@@ -0,0 +1,7 @@
+export interface IChatKitAgentProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/chatKitAgent/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IChatKitAgentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ChatKitAgentWebPartStrings' {
+  const strings: IChatKitAgentWebPartStrings;
+  export = strings;
+}
